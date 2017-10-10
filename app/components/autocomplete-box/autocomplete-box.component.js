962db57045911d205fcbab01ef516bb04d02e324
Dylan Smith <dylantsmith@gmail.com>
2017-10-09 22:13:32 -0500

    Show/hide options based on text input
    - Modify the component to be stateful, and display the options list
    only if there is user input.

--- app/components/autocomplete-box/autocomplete-box.component.js
+++ app/components/autocomplete-box/autocomplete-box.component.js
@@ -1,35 +1,55 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
 
 import style from './autocomplete-box.component.scss';
 
-export default function AutocompleteBox({ options }) {
-  const optionsToRender = options.map(option => (
-    <div className={style.option} key={option.username}>
-      <div className={style.avatarContainer}>
-        <img
-          className={style.avatar}
-          src={option.avatar}
-          alt=""
-        />
-      </div>
-      <div className={style.username}>{option.username}</div>
-      <div className={style.name}>{option.name}</div>
-    </div>
-  ));
+export default class AutocompleteBox extends Component {
+  state = {
+    userText: '',
+  }
 
-  return (
-    <div>
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  render() {
+    let optionsToRender;
+
+    if (this.state.userText) {
+      optionsToRender = this.props.options.map(option => (
+        <div className={style.option} key={option.username}>
+          <div className={style.avatarContainer}>
+            <img
+              className={style.avatar}
+              src={option.avatar}
+              alt=""
+            />
+          </div>
+          <div className={style.username}>{option.username}</div>
+          <div className={style.name}>{option.name}</div>
+        </div>
+      ));
+    }
+
+    return (
       <div>
-        <textarea
-          className={style.textarea}
-        />
+        <div>
+          <textarea
+            className={style.textarea}
+            name="userText"
+            placeholder="What's happening?"
+            value={this.state.userText}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div className={style.options}>
+          {optionsToRender}
+        </div>
       </div>
-      <div className={style.options}>
-        {optionsToRender}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 AutocompleteBox.propTypes = {
